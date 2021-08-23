cdb3cc8e83c319ce4be95635d8356a139fb1a298
hzviera <88221181+hzviera@users.noreply.github.com>
2021-08-23 08:32:04 -0300

    User Register

---
+++ IdentityFramework/Models/AccountLoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityFramework.Models
+{
+    public class AccountLoginViewModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Email in invald format")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
